495ba661915bbb852f53459496cca0a7e804bfaa
Chung Nguyen <zrmedia9@gmail.com>
2024-04-13 11:02:28 +0700

    Update tailwind.config.js, main.js, input.css, and index.html

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ const toggleTopMenuIcon = document.getElementById('ct-toggle-top-menu-icon');
 document.addEventListener('click', (event) => {
   if (toggleTopMenuIcon.contains(event.target)) {
     topMenu.classList.toggle('hidden');
+    topMenu.classList.toggle('ct-topmenu-expanded');
   } else {
+    if (topMenu.classList.contains('ct-topmenu-expanded')) {
+      topMenu.classList.remove('ct-topmenu-expanded');
+      topMenu.classList.add('hidden');
+    }
   }
 });
